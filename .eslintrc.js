2cfa94ba0be18c453e7c5702d11d5e586524b395
Katarina Ljungdahl <katarina@folkungagatan54.se>
2017-04-28 14:04:59 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "parserOptions": {
+      "sourceType": "module"
+    },
+    "extends": "airbnb-base",
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "one-var": "off",
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-var": "off",
+        "comma-dangle": [
+          "error",
+          "never"
+        ],
+        "prefer-arrow-callback": "off",
+        "strict": [
+          "error",
+          "global"
+        ],
+        "no-plusplus": [
+          "error",
+          { "allowForLoopAfterthoughts": true }
+        ],
+        "prefer-template": "off",
+        "no-restricted-syntax": "off",
+        "func-names": "off"
+    }
+};
