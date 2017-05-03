c42d99e48cd80dd9248cfafb7f20ff5bca32920e
Tuukka Teriö <tuukka.terio@medieinstitutet.se>
2017-05-03 14:42:57 +0200

    Score works (with jQuery)

--- src/server/views/partials/game-container.js
+++ src/server/views/partials/game-container.js
@@ -2,7 +2,8 @@ module.exports = function gameContainer() {
   return `
   <div class="gameContainer gameContainer-js">
     <p>Press space or arrow up to jump</p>
-    <div class="scoreContainer">Score:<span class="scoreSpan">0</span></div>
+    <div class="high"></div>
+    <div>Score:<span class="scoreSpan">0</span></div>
     <div class="player"></div>
   </div>
   `;
