070dd37589492b538a2ce0f4b555deabcdbcbdf1
Katarina Ljungdahl <katarina@folkungagatan54.se>
2017-05-05 14:19:09 +0200

    Changed order of things, now user is saved only when logging in, not every time a new highscore occurs

--- src/scripts/socket.js
+++ src/scripts/socket.js
@@ -2,14 +2,9 @@ export const socket = io();
 
 /**
  * Send information about new high score and user id to server.
- * @param  {number} score The new high score.
+ * @param  {object}   user    The current user
+ * @param  {number}   score   The new high score.
  */
-export function sendScore(score) {
-  // TODO: Move this to main.js, and make sendScore take user as argument.
-  fetch('/user', { credentials: 'include', headers: { accept: 'application/json' } })
-    .then(body => body.json())
-    .then((user) => {
-      socket.emit('on-highscore', { id: user.id, highscore: score });
-    })
-    .catch(err => console.error(err));
+export function sendScore(user, score) {
+  socket.emit('on-highscore', { id: user.id, highscore: score });
 }
