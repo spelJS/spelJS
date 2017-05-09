ed62d50997b27fc5ad05379ff5c3f69c78b453a9
Tuukka Teriö <tuukka.terio@medieinstitutet.se>
2017-05-09 13:23:15 +0200

    Start Page styling and font ’Iceland’

--- src/server/views/partials/header.js
+++ src/server/views/partials/header.js
@@ -7,6 +7,7 @@ module.exports = function header() {
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta name="description" content="JavaScript Inl 3 -­ Kampanj">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <link href="https://fonts.googleapis.com/css?family=Iceland" rel="stylesheet">
       <link rel="stylesheet" type="text/css" href="/style.css">
       <title>Laika's Space Adventure</title>
     </head>
