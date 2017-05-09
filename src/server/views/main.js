1589b3189e28d0369cf2eaf8f72bb8e66dd11560
Tuukka Teriö <tuukka.terio@medieinstitutet.se>
2017-05-09 14:38:26 +0200

    Styling for user-info and gameInfo

--- src/server/views/main.js
+++ src/server/views/main.js
@@ -15,13 +15,18 @@ module.exports = function mainView(req) {
           `
           :
           `
-            <h1 class="headline">Laika's Space Adventure</h1>
             <div class="user-info">
               <div class="user-info__content">
                 <p>You are logged in as: ${req.user.name}</p>
                 <a href="/logout">Logout</a>
               </div>
               <img src="${req.user.photo}" alt="Photo of ${req.user.name}">
+            </div>
+            <div class="gameInfo">
+              <div class="highscore"></div>
+              <p class="scoreText">Your High Score: <span class="highSpan-js">${req.user.highscore}</span></p>
+              <p class="scoreText">Score: <span class="scoreSpan-js">0</span></p>
+              <p>Press space or arrow up to jump</p>
             </div>
             ${gameContainer(req.user.highscore)}
           `
