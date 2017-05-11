5aedfc5f2bb2b822b0a62d58eeb1def8c7d0f574
Katarina Ljungdahl <katarina@folkungagatan54.se>
2017-05-11 15:38:26 +0200

    Work in progress, trying to sort out highscores

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,11 +5,12 @@ import { listenOnHighscore } from './socket';
 
 const content = document.querySelector('.content-js');
 const gameContainer = document.querySelector('.gameContainer-js');
+const scoreContainer = document.querySelector('.highscoreContainer-js');
 
 // Updated URL and initialize game when user is logged in
 if (gameContainer) {
   updateURL(window.location.href);
-  fetch('/user', { credentials: 'include', headers: { accept: 'application/json' } })
+  fetch('/getuser', { credentials: 'include', headers: { accept: 'application/json' } })
     .then(body => body.json())
     .then((user) => {
       const game = initGame(gameContainer, user);
@@ -17,5 +18,15 @@ if (gameContainer) {
     })
     .catch(err => console.error(err));
 }
+
+if (scoreContainer) {
+  fetch('/getscore', { credentials: 'include', headers: { accept: 'application/json' } })
+  .then(body => body.json())
+    .then((user) => {
+      console.log(user);
+    })
+    .catch(err => console.error(err));
+}
+
 
 listenOnHighscore();
