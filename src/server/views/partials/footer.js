bdb23488b4d773826aec7dcc86abc56bc8d53d83
Katarina Ljungdahl <katarina@folkungagatan54.se>
2017-05-16 11:26:22 +0200

    Sorted out bug with z-index

--- src/server/views/partials/footer.js
+++ src/server/views/partials/footer.js
@@ -1,6 +1,6 @@
 module.exports = function footer() {
   return `
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdn.polyfill.io/v2/polyfill.min.js?features=default,fetch"></script>
     <script src="/socket.io/socket.io.js"></script>
     <script src="/main.js"></script>
     </body>
