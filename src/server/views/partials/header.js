b18b59f7a8155342caefbd5f5509d7f2781c1e4e
Katarina Ljungdahl <katarina@folkungagatan54.se>
2017-05-16 13:51:53 +0200

    Added manifest for webapp and favicons

--- src/server/views/partials/header.js
+++ src/server/views/partials/header.js
@@ -5,10 +5,18 @@ module.exports = function header() {
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta name="description" content="JavaScript Inl 3 -­ Kampanj">
+      <meta name="description" content="Laika's Space Adventure">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <link href="https://fonts.googleapis.com/css?family=Iceland" rel="stylesheet">
-      <link rel="stylesheet" type="text/css" href="/style.css">
+
+      <link rel="stylesheet" type="text/css" href="style.css">
+      <link rel="apple-touch-icon" sizes="180x180" href="img/favicons/apple-touch-icon.png">
+      <link rel="icon" type="image/png" sizes="32x32" href="img/favicons/favicon-32x32.png">
+      <link rel="icon" type="image/png" sizes="16x16" href="img/favicons/favicon-16x16.png">
+      <link rel="mask-icon" href="img/favicons/safari-pinned-tab.svg" color="#999999">
+
+      <link rel="manifest" href="manifest.json">
+      <meta name="theme-color" content="#0c090d">
+
       <title>Laika's Space Adventure</title>
     </head>
     <body>
