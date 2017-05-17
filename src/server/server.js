b19677bb2060eea81391d91d0ca32250a3339fb3
Tuukka Teriö <tuukka.terio@medieinstitutet.se>
2017-05-17 16:21:03 +0200

    Edited server.js for testing purposes.

--- src/server/server.js
+++ src/server/server.js
@@ -6,10 +6,14 @@ const mainView = require('./views/main');
 const scoreView = require('./views/score');
 const loginView = require('./views/login');
 
+/*
 // Secret variables stored on Heroku
 const clientID = process.env.clientID;
 const clientSecret = process.env.clientSecret;
 const callbackURL = process.env.callbackURL;
+*/
+
+const { clientID, clientSecret, callbackURL } = require('./credentials');
 
 // The functions for handling user database.
 const {
