2d746bdf561fdfe37f5af5e334ba93b5d65047ab
Katarina Ljungdahl <katarina@folkungagatan54.se>
2017-05-18 14:04:58 +0200

    Styled game instructions

--- src/server/views/partials/game-container.js
+++ src/server/views/partials/game-container.js
@@ -2,9 +2,9 @@ const player = require('./player');
 
 module.exports = function gameContainer() {
   return `
-  <p class="headline instructions gameInstructions-js"></p>
   <div class="gameContainer gameContainer-js">
     <div class="player player-js">${player()}</div>
   </div>
+  <p class="headline instructions gameInstructions-js"></p>
   `;
 };
